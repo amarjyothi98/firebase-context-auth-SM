41a6143227d38c3ea098339a65931c1690b9cceb
amarjyothi98 <amarjyothi526@gmail.com>
2023-05-04 23:12:56 +0530

    redux-project also stopped - now transitioning to the context api: '

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getAuth} from "firebase/auth"
+import { googleAuthProvider } from "firebase/auth"
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDO2OEoOQb6JjUsQ4sR_WCR3rWzLvnXvW0",
   authDomain: "redux-auth-social-media.firebaseapp.com",
@@ -18,4 +15,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const auth = getAuth(); 
+const provider = googleAuthProvider(); 
+
+export { app, provider, auth }; 
+
