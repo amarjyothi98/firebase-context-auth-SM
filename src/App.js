b63d71492fa1a81b972b6bc8f0655bb8a501005a
amarjyothi98 <amarjyothi526@gmail.com>
2023-05-05 08:15:44 +0530

    setup and pages created with routing and navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login'
+import Signup from './components/Signup'
+import { Col, Container, Row } from 'react-bootstrap';
+
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <>
+    <Router>
+    <Container>
+      <Row>
+        <Col>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/signup' element={<Signup/>}/>
+        </Routes>
+        </Col>
+      </Row>
+    </Container>
+    </Router>
+    </>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import { Form, Alert } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import GoogleButton from "react-google-button";
+import { Link } from 'react-router-dom';
+import '../App.css'
+
+
+
+export default function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleGoogleSignIn = () => {
+    
+  }
+
+
+  return (
+    <>
+    <div className="p-4 box">
+      <h2 className="mb-3">Firebase Auth Login</h2>
+      {error && <Alert variant="danger">{error}</Alert>}
+      <Form >
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control
+            type="email"
+            placeholder="Email address"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+
+        <div className="d-grid gap-2">
+          <Button variant="primary" type="Submit">
+            Log In
+          </Button>
+        </div>
+      </Form>
+      <hr />
+      <div>
+        <GoogleButton
+          className="g-btn"
+          type="dark"
+          onClick={handleGoogleSignIn}
+        />
+      </div>
+    </div>
+    <div className="p-4 box mt-3 text-center">
+      Don't have an account? <Link to="/signup">Sign up</Link>
+    </div>
+  </>
+  )
+}
